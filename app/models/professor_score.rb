bf82867c4ef6cfb6817edae18fba1b147e17e960
Aaron Wishnick <aaronwishnick@gmail.com>
2014-04-08 22:22:17 -0400

    add caching on subtable queries

--- app/models/professor_score.rb
+++ app/models/professor_score.rb
@@ -61,64 +61,64 @@ class ProfessorScore < ActiveRecord::Base
   end
 
   def self.get_professor_data(id)
-    # prof_data = Rails.cache.read(self.course_id.to_s + "profdata")
-    # if !prof_data
-    ProfessorScore.select("AVG(professor_scores.score) as average_professor_score,"+
-                          "AVG(course_scores.score) as average_course_score,"+
-                          "professor_scores.course_id, courses.course_num,"+
-                          " courses.name as course_name, professor_scores.professor_id,"+
-                          " professors.utln", "professors.name as professor_name")
-    .group(['professor_scores.course_id', 'courses.course_num','courses.name', 'professor_scores.professor_id',
-            'professors.utln', 'professors.name'])
-    .where('professor_scores.course_id = ?', id)
-    .joins("INNER JOIN course_scores ON professor_scores.professor_id "+
-           "= course_scores.professor_id and professor_scores.course_id "+
-           "= course_scores.course_id and professor_scores.semester_id ="+
-           " course_scores.semester_id")
-    .joins(:professor,:course,:semester)
-    # Rails.cache.write(self.course_id.to_s + "profdata", prof_data)
-    # end
-    # prof_data
+    prof_data = Rails.cache.read(id.to_s + "profdata")
+    if !prof_data
+      prof_data = ProfessorScore.select("AVG(professor_scores.score) as average_professor_score,"+
+                                        "AVG(course_scores.score) as average_course_score,"+
+                                        "professor_scores.course_id, courses.course_num,"+
+                                        " courses.name as course_name, professor_scores.professor_id,"+
+                                        " professors.utln", "professors.name as professor_name")
+      .group(['professor_scores.course_id', 'courses.course_num','courses.name', 'professor_scores.professor_id',
+              'professors.utln', 'professors.name'])
+      .where('professor_scores.course_id = ?', id)
+      .joins("INNER JOIN course_scores ON professor_scores.professor_id "+
+             "= course_scores.professor_id and professor_scores.course_id "+
+             "= course_scores.course_id and professor_scores.semester_id ="+
+             " course_scores.semester_id")
+      .joins(:professor,:course,:semester)
+      Rails.cache.write(id.to_s + "profdata", prof_data)
+    end
+    prof_data
   end
 
   def get_semester_data
-    # semester_data = Rails.cache.read(self.professor_id.to_s + "-" + self.course_id.to_s + "sem_data")
-    # if !semester_data
-    ProfessorScore.select("AVG(professor_scores.score) as average_professor_score,"+
-                          "AVG(course_scores.score) as average_course_score,"+
-                          "professor_scores.course_id,professor_scores.semester_id,"+
-                          " professor_scores.professor_id, semesters.name as semester_name")
-    .group(['professor_scores.course_id',  'professor_scores.professor_id', 'professor_scores.semester_id','semesters.name'])
-    .where('professor_scores.professor_id = ? AND professor_scores.course_id = ?', self.professor_id, self.course_id)
-    .joins("INNER JOIN course_scores ON professor_scores.professor_id "+
-           "= course_scores.professor_id and professor_scores.course_id "+
-           "= course_scores.course_id and professor_scores.semester_id ="+
-           " course_scores.semester_id")
-    .joins(:professor,:course,:semester)
-    #   Rails.cache.write(self.professor_id.to_s + "-" + self.course_id.to_s + "sem_data", semester_data)
-    # end
-    # semester_data
+    semester_data = Rails.cache.read(self.professor_id.to_s + "-" + self.course_id.to_s + "sem_data")
+    if !semester_data
+      semester_data = ProfessorScore.select("AVG(professor_scores.score) as average_professor_score,"+
+                                            "AVG(course_scores.score) as average_course_score,"+
+                                            "professor_scores.course_id,professor_scores.semester_id,"+
+                                            " professor_scores.professor_id, semesters.name as semester_name")
+      .group(['professor_scores.course_id',  'professor_scores.professor_id', 'professor_scores.semester_id','semesters.name'])
+      .where('professor_scores.professor_id = ? AND professor_scores.course_id = ?', self.professor_id, self.course_id)
+      .joins("INNER JOIN course_scores ON professor_scores.professor_id "+
+             "= course_scores.professor_id and professor_scores.course_id "+
+             "= course_scores.course_id and professor_scores.semester_id ="+
+             " course_scores.semester_id")
+      .joins(:professor,:course,:semester)
+      Rails.cache.write(self.professor_id.to_s + "-" + self.course_id.to_s + "sem_data", semester_data)
+    end
+    semester_data
   end
 
   def self.get_course_data(id)
-    # course_data = Rails.cache.read(self.professor_id.to_s + "coursedata")
-    # if !course_Data
-    ProfessorScore.select("AVG(professor_scores.score) as average_professor_score,"+
-                          "AVG(course_scores.score) as average_course_score,"+
-                          "professor_scores.course_id, courses.course_num,"+
-                          " courses.name as course_name, professor_scores.professor_id,"+
-                          " professors.utln, professors.name as professor_name")
-    .group(['professor_scores.course_id', 'courses.course_num','courses.name', 'professor_scores.professor_id',
-            'professors.utln', 'professors.name'])
-    .where('professor_scores.professor_id = ?', id)
-    .joins("INNER JOIN course_scores ON professor_scores.professor_id "+
-           "= course_scores.professor_id and professor_scores.course_id "+
-           "= course_scores.course_id and professor_scores.semester_id ="+
-           " course_scores.semester_id")
-    .joins(:professor,:course,:semester)
-    #   Rails.cache.write(self.professor_id.to_s + "coursedata", course_data)
-    # end
-    # course_data
+    course_data = Rails.cache.read(id.to_s + "coursedata")
+    if !course_Data
+      course_data = ProfessorScore.select("AVG(professor_scores.score) as average_professor_score,"+
+                                          "AVG(course_scores.score) as average_course_score,"+
+                                          "professor_scores.course_id, courses.course_num,"+
+                                          " courses.name as course_name, professor_scores.professor_id,"+
+                                          " professors.utln, professors.name as professor_name")
+      .group(['professor_scores.course_id', 'courses.course_num','courses.name', 'professor_scores.professor_id',
+              'professors.utln', 'professors.name'])
+      .where('professor_scores.professor_id = ?', id)
+      .joins("INNER JOIN course_scores ON professor_scores.professor_id "+
+             "= course_scores.professor_id and professor_scores.course_id "+
+             "= course_scores.course_id and professor_scores.semester_id ="+
+             " course_scores.semester_id")
+      .joins(:professor,:course,:semester)
+      Rails.cache.write(id.to_s + "coursedata", course_data)
+    end
+    course_data
   end
 
   def expire_cache
